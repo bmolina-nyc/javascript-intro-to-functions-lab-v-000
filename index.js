17b40e47a7a57985ca67927b5ad106401f22e615
bmolina-nyc <bruce.molina.81@gmail.com>
2017-02-20 18:54:28 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string){
+  console.log(whisper(string))
+}
+
+
+function sayHiToGrandma(string){
+  if (string === "I love you, Grandma."){
+    return "I love you, too."
+  } else if (string === (shout(string)) ) {
+    return "YES INDEED!"
+  } else {
+    return "I can't hear you!"
+  }
+}
+
+  // it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+  //   expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  // })
+
+  // it('returns "YES INDEED!" if `string` is uppercase', function() {
+  //   expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  // })
+
+  // it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+  //   expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  // })
